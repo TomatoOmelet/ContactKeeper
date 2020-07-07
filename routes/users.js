ccfd7a8c09b3894227b326f0c949d6f7c7635839
Weiyan Zhu <weiyanz@uci.edu>
2020-07-07 14:19:44 -0700

    create user model and its validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+const user = require("../models/User")
 
 //@route    POST api/users
 //@desc     Register a user
 //@access   Public
-router.post("/", (req, res) => {
-    res.send("Register a User")
+router.post("/", [
+    body("name", "Name is required").notEmpty(),
+    body("email", "Email is not valid").isEmail(),
+    body("password", "Password needs to have 6 or more characters").isLength({min:6})
+],(req, res) => {
+    const error = validationResult(req)
+    if(!error.isEmpty())
+    {
+        return res.status(400).json({errors: error.array()})
+    }
+    res.send("passed")
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express()
 //Connect DataBase
 connectDB()
 
+//init middleware
+app.use(express.json({extended:false}));
 
 app.get("/", (req, res) =>{res.json({message: "Hello"})})
 
