4419916e3e748017b63f307802c485ddd6b6d789
Weiyan Zhu <weiyanz@uci.edu>
2020-07-08 10:30:14 -0700

    get  contacts for logged in user

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const auth = require("../middleware/auth");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 //@route    Get api/contacts
 //@desc     Get all user's contacts
 //@access   Private
-router.get("/", (req, res) => {
-    res.send("Get all contacts")
+router.get("/", auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date:-1});
+        res.json(contacts);
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error")
+    }
 });
 
 //@route    Post api/contacts
