cdf82b0899252bb30df5c3d71c13df6e8e866688
Weiyan Zhu <weiyanz@uci.edu>
2020-07-08 17:00:55 -0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
   return (
-    <div className="App">
-      APP
-    </div>
+    <BrowserRouter>
+      <Fragment className="App">
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component = {Home}/>
+            <Route exact path="/About" component = {About}/>
+          </Switch>
+        </div>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
