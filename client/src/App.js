f9e72b368ec51719621c936c507a8249d5f7cd67
Weiyan Zhu <weiyanz@uci.edu>
2020-07-09 14:46:02 -0700

    login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
+import Register from "./components/auth/Register"
+import Login from "./components/auth/Login"
+
 import ContactState from "./context/contact/ContactState"
 import AuthState from "./context/auth/AuthState"
 
@@ -19,6 +22,8 @@ function App() {
           <Switch>
             <Route exact path="/" component = {Home}/>
             <Route exact path="/About" component = {About}/>
+            <Route exact path="/Register" component = {Register}/>
+            <Route exact path="/Login" component = {Login}/>
           </Switch>
         </div>
       </Fragment>
