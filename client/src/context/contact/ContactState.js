b50fcc92bc58a3b7a24048ff7501999a067ffcb3
Weiyan Zhu <weiyanz@uci.edu>
2020-07-10 10:34:08 -0700

    get contacts from database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,17 +2,33 @@ import React, {useReducer} from "react"
 import axios from "axios"
 import contactContext from "./contactContext"
 import contactReducer from "./contactReducer"
-import {ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, FILTER_CONTACTS, CLEAR_FILTER, UPDATE_CONTACT, CONTACT_ERROR} from "../types"
+import {ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, FILTER_CONTACTS, CLEAR_FILTER, UPDATE_CONTACT, CONTACT_ERROR,
+    CLEAR_CONTACTS, GET_CONTACTS} from "../types"
 
 const ContactState = (props)=>{
     const initialState = {
-        contacts:[],
+        contacts:null,
         current : null,
         filtered: null,
         error:null
     }
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
+    //Get Contact
+    const getContacts= async ()=>{
+        try {
+            const res = await axios.get("/api/contacts");
+            dispatch({type:GET_CONTACTS, payload:res.data})
+        } catch (error) {
+            dispatch({type:CONTACT_ERROR, payload:error.response.msg})
+        }
+        
+    }
+
+    //Clear Contact
+    const clearContacts= ()=>{
+        dispatch({type:CLEAR_CONTACTS})        
+    }
 
     //Add Contact
     const addContact= async (contact)=>{
@@ -62,6 +78,8 @@ const ContactState = (props)=>{
             current: state.current,
             filtered: state.filtered,
             error:state.error,
+            getContacts,
+            clearContacts,
             addContact,
             deleteContact,
             setCurrent,
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,21 +1,39 @@
-import {ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, FILTER_CONTACTS, CLEAR_FILTER, UPDATE_CONTACT, CONTACT_ERROR} from "../types"
+import {ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, FILTER_CONTACTS, CLEAR_FILTER, UPDATE_CONTACT, CONTACT_ERROR,
+    CLEAR_CONTACTS, GET_CONTACTS} from "../types"
 
 export default (state, action) =>{
     switch(action.type){
+        case GET_CONTACTS:
+            return{
+                ...state,
+                contacts: action.payload,
+                loading: false
+            }
+        case CLEAR_CONTACTS:
+            return{
+                ...state,
+                contacts: null,
+                filtered: null,
+                error:null,
+                current:null
+            }
         case ADD_CONTACT:
             return{
                 ...state,
-                contacts: [...state.contacts, action.payload]
+                contacts: [...state.contacts, action.payload],
+                loading: false
             }
         case UPDATE_CONTACT:
             return{
                 ...state,
-                contacts: state.contacts.map(contact=>contact.id === action.payload.id?action.payload:contact)
+                contacts: state.contacts.map(contact=>contact.id === action.payload.id?action.payload:contact),
+                loading: false
             }
         case DELETE_CONTACT:
             return{
                 ...state,
-                contacts: state.contacts.filter(contact=>contact.id !== action.payload)
+                contacts: state.contacts.filter(contact=>contact.id !== action.payload),
+                loading: false
             }
         case SET_CURRENT:
             return{
